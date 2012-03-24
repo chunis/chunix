2e021f20765933fa3c7c0859d6e05e3a615f700b
Chunis Deng <chunchengfh@gmail.com>
2012-03-24 17:31:04 +0800

    Release Chunix-0.0.1

--- kern/sys.h
+++ kern/sys.h
@@ -1,8 +1,9 @@
 #ifndef __SYS_H__
 #define __SYS_H__
 
-char *sys_version = "0.0.1-dev";
+char *sys_version = "0.0.1";
 char *sys_info = "Author: Chunis Deng (chunchengfh@gmail.com)\n"
-			"Url: http://code.google.com/p/chunix/";
+			"Code: http://code.google.com/p/chunix/\n"
+			"Site: http://www.chunix.org";
 
 #endif
