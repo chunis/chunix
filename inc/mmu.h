bf5a551c7364ed26e4292486b0f314e13d3fee37
Chunis Deng <chunchengfh@gmail.com>
2012-06-12 19:31:03 +0800

    add mmu.h for mm

---
+++ inc/mmu.h
@@ -0,0 +1,32 @@
+#ifndef __MMU_H__
+#define __MMU_H__
+
+#define PGSIZE		4096
+#define PTXSHIFT	12
+#define PDXSHIFT	22
+
+#define KERNBASE	0x80000000
+
+// page dir/table entry flags
+#define PTE_P		0x001	// present
+#define PTE_W		0x002	// writable
+#define PTE_U		0x004	// user
+#define PTE_A		0x020	// accessed
+#define PTE_D		0x040	// dirty
+
+// page dir index
+#define PTX(va)		(((uint32_t)(va) >> PTXSHIFT) & 0x3ff)
+// page table index
+#define PDX(va)		(((uint32_t)(va) >> PDXSHIFT) & 0x3ff)
+// assemble page address from d, t and o
+#define PGADDR(d, t, o)	((uint32_t)((d) <<PDXSHIFT | (t) << PTXSHIFT | (o)))
+// Address in page table or page directory entry
+#define PTE_ADDR(pte)	((uint32_t)(pte) & ~0xFFF)
+
+#define V2P(a)		((a) - KERNBASE)
+#define P2V(a)		((a) + KERNBASE)
+
+#define PGROUNDUP(a)	(((a)+PGSIZE-1) & ~(PGSIZE-1))
+#define PGROUNDDOWN(a)	(((a)) & ~(PGSIZE-1))
+
+#endif
--- inc/types.h
+++ inc/types.h
@@ -9,5 +9,7 @@ typedef int int32_t;
 typedef unsigned int uint32_t;
 typedef long long int64_t;
 typedef unsigned long long uint64_t;
-    
+
+typedef uint32_t pte_t;
+
 #endif
