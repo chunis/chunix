d26227c15e96845824679022e54117c75003c9ab
Chunis Deng <chunchengfh@gmail.com>
2012-09-14 14:28:24 +0800

    add macro DBGx() to make debug a little easier

--- kern/printf.h
+++ kern/printf.h
@@ -4,4 +4,12 @@
 void printf(char *fmt, ...);
 void panic(char *str);
 
+#define DBGx(x)  (printf("DBG ---> '%s:%d': " #x " = %x\n", \
+			__FILE__, __LINE__, x))
+#define DBGd(x) (printf("DBG ---> '%s:%d': " #x " = %d\n", \
+			__FILE__, __LINE__, x))
+#define DBGs(x) (printf("DBG ---> '%s:%d': " #x " = %s\n", \
+			__FILE__, __LINE__, x))
+#define DBG(x)	DBGx(x)
+
 #endif
