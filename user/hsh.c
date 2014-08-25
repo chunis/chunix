3f6c2b1fba2d83c5727f3c9e9be3a4b94b2a930e
Chunis Deng <chunchengfh@gmail.com>
2014-08-25 22:04:44 +0800

    obtain arguments from command for hsh

--- user/hsh.c
+++ user/hsh.c
@@ -2,44 +2,114 @@
 
 #include <user.h>
 
+#define MAXARGS 16
+#define BUFZ	1024
 
-hsh_version = "V0.01-dev @ 2014-07-10";
+char *hsh_version = "V0.01-dev @ 2014-07-10";
+char buf[BUFZ];
 
-void help(void)
+static void help(void)
 {
 	printf("    help\tThis help\n");
 	printf("    hello\tRun Hello\n");
 	printf("    version\tGet Hsh's version info\n");
 }
 
-void get_hsh_version(void)
+static void get_hsh_version(void)
 {
 	printf("    hsh %s\n", hsh_version);
 }
 
-void clean_screen(void)
+static void clean_screen(void)
 {
 	// TODO
 	printf("(Run command 'clr')\n");
 	return 0;
 }
 
-int run_cmd(char *cmd)
+
+char *str_forward(char **start, char *end)
 {
-	printf("(Run command '%s')\n", cmd);
+	char *p, *s;
+
+	p = *start;
+	while((p < end) && (*p == ' ' || *p == '\t'))
+		p++;
+	s = p;
+
+	while(*p != ' ' && *p != '\t' && p < end)
+		p++;
+
+	*start = p;
+	return s;
+}
+
+void split_args(char *str, char **buf, int maxn)
+{
+	char *p, *q;
+	int i = 0;
+
+	q = str + strlen(str);
+	p = str_forward(&str, str+strlen(str));
+	while(p < q){
+		if(++i >= maxn){
+			printf("Too many arguments!\n");
+			//exit(-1);
+		}
+		*buf = p;
+		*buf++;
+		*str++ = '\0';
+		p = str_forward(&str, str+strlen(str));
+	}
+	*buf = '\0'; // add 'NULL' as the last element of **buf
+}
+
+int check_xargv(char *xargv[])
+{
+	int argc = 0;
+	char **p = xargv;
+
+	while(*p){
+		argc++;
+		printf("%d: %s\n", argc, *p++);
+	}
+	printf("argc = %d\n", argc);
+
 	return 0;
+}
+
+static void run_cmd(char *cmd)
+{
+	char *p, *xargv[MAXARGS];
+	int pid;
+
+	printf("(Run command '%s')\n", cmd);
+	split_args(cmd, xargv, MAXARGS);
+	check_xargv(xargv);
+
+#if 0
+	pid = fork();
+	if(pid == -1){
+		printf("fork() failed\n");
+		exit();
+	}
+	else if(pid == 0){
+		exec(xargv[0], xargv);
+		printf("cmd '%s' failed\n", xargv[0]);
+	}
+	wait();
+#endif
 }
 
 int main(int argc, char *argv[])
 {
-	int ret;
-	char buf[1024];
+	int ret = 0;
 
 	for(;;){
 		printf("hsh > ");
 
-		//memset(buf, 0, 1024);
-		gets(buf, 1024);
+		//memset(buf, 0, BUFZ);
+		gets(buf, BUFZ);
 		buf[strlen(buf)-1] = '\0';
 		//printf("You Entered: |%s|\n", buf);
 		if(strcmp(buf, "help") == 0){
@@ -48,15 +118,14 @@ int main(int argc, char *argv[])
 			get_hsh_version();
 		} else if(strcmp(buf, "clr") == 0){
 			clean_screen();
-		} else if(strcmp(buf, "hello") == 0){
-			ret = run_cmd(buf);
-			if(ret){
-				printf("cmd '%s' failed (ret = %d)\n", ret);
-			}
+		} else if(strncmp(buf, "hello", 5) == 0
+			|| strncmp(buf, "echo", 4) == 0){
+			run_cmd(buf);
 		} else if(strlen(buf)){
 			printf("command '%s' not found\n", buf);
+			ret = -1;
 		}
 	}
 
-	return 0;
+	return ret;
 }
